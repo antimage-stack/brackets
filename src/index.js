1f60f4b98059c07cab6930a76490db9731aa92c3
antimage-stack <vanua.osipovporg@gmail.com>
2021-02-22 01:00:40 +0300

    docs: finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if ( str.length % 2 !== 0 ) return false
+
+  for ( let i = 0; i < bracketsConfig.length; i++ ) {
+    let pair = bracketsConfig[i].join('')
+
+    if ( str.includes(pair) ) {
+      str = str.replace(pair, '')
+      i = -1
+    }
+  }
+  return str.length === 0
 }
